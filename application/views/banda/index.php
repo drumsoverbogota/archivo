93f2cddb810edf11797fee71ba45eb0bdc8a6e04
Sergio Mancera <sergiomanceranom@gmail.com>
2018-05-28 09:54:37 -0500

    Agregada la funcionalidad de mostrar las bandas como si fuera una lista

--- application/views/banda/index.php
+++ application/views/banda/index.php
@@ -1,12 +1,16 @@
 <h2><?php echo $title; ?></h2>
 
 <ul>
-	<li>
-	<?php foreach ($banda as $banda_item): ?>	
+	
+	<?php foreach ($banda as $banda_item): ?>
+		<li>	
 		<a href="<?php echo site_url('banda/'.$banda_item['id']); ?>"><?php echo $banda_item['nombre']; ?></a>
-		<?php echo $banda_item['otros']; ?>
+		<?php if ($banda_item['otros']){?>
+			(<?php echo str_replace("\n", ",", $banda_item['otros']); ?>)
+		<?php }?>
+		</li>
 	<?php endforeach; ?>
-	</li>
+	
 </ul>
 
 </table>
