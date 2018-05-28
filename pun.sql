92449498cbdd9a0e05a5916a7ab924c80b7170b0
Sergio Mancera <sergiomanceranom@gmail.com>
2018-05-27 23:03:48 -0500

    Agregado un campo para las bandas que son extranjeras

--- pun.sql
+++ pun.sql
@@ -1,14 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 25, 2018 at 06:12 PM
--- Server version: 5.7.22-0ubuntu0.16.04.1
--- PHP Version: 7.0.30-0ubuntu0.16.04.1
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 28-05-2018 a las 04:02:42
+-- Versión del servidor: 5.7.19
+-- Versión de PHP: 5.6.31
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -18,43 +20,50 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pun`
+-- Base de datos: `pun`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `banda`
+-- Estructura de tabla para la tabla `banda`
 --
 
-CREATE TABLE `banda` (
-  `id` int(100) NOT NULL,
+DROP TABLE IF EXISTS `banda`;
+CREATE TABLE IF NOT EXISTS `banda` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   `otros` text NOT NULL,
   `integrantes` text,
   `comentarios` text,
-  `imagen` text
+  `imagen` text,
+  `extranjera` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `banda_lanzamiento`
+-- Estructura de tabla para la tabla `banda_lanzamiento`
 --
 
-CREATE TABLE `banda_lanzamiento` (
+DROP TABLE IF EXISTS `banda_lanzamiento`;
+CREATE TABLE IF NOT EXISTS `banda_lanzamiento` (
   `banda_id` int(11) NOT NULL,
-  `lanzamiento_id` int(11) NOT NULL
+  `lanzamiento_id` int(11) NOT NULL,
+  KEY `banda_lanzamiento_ibfk_1` (`banda_id`),
+  KEY `lanzamiento_id` (`lanzamiento_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `lanzamiento`
+-- Estructura de tabla para la tabla `lanzamiento`
 --
 
-CREATE TABLE `lanzamiento` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `lanzamiento`;
+CREATE TABLE IF NOT EXISTS `lanzamiento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` text NOT NULL,
   `referencia` text,
   `formato` enum('CD','Digipack','12"','10"','7"','Flexi','Cassette','Digital','Mini CD','DVD','Otros') DEFAULT NULL,
@@ -63,57 +72,22 @@ CREATE TABLE `lanzamiento` (
   `creditos` text,
   `notas` text,
   `link` text,
-  `imagen` text
+  `imagen` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Indexes for table `banda`
---
-ALTER TABLE `banda`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `banda_lanzamiento`
---
-ALTER TABLE `banda_lanzamiento`
-  ADD KEY `banda_lanzamiento_ibfk_1` (`banda_id`),
-  ADD KEY `lanzamiento_id` (`lanzamiento_id`);
-
---
--- Indexes for table `lanzamiento`
---
-ALTER TABLE `lanzamiento`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `banda`
---
-ALTER TABLE `banda`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `lanzamiento`
---
-ALTER TABLE `lanzamiento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `banda_lanzamiento`
+-- Filtros para la tabla `banda_lanzamiento`
 --
 ALTER TABLE `banda_lanzamiento`
   ADD CONSTRAINT `banda_lanzamiento_ibfk_1` FOREIGN KEY (`banda_id`) REFERENCES `banda` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `banda_lanzamiento_ibfk_2` FOREIGN KEY (`lanzamiento_id`) REFERENCES `lanzamiento` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
