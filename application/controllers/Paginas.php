3b45d9af4a84772c4275ff789116d4b85f84a65e
Sergio Mancera <sergio@MacBook-Air-de-Sergio.local>
2018-08-14 08:23:43 -0500

    Añadidas cosas para el formulario de contacto

--- application/controllers/Paginas.php
+++ application/controllers/Paginas.php
@@ -5,12 +5,12 @@ class Paginas extends CI_Controller {
         {
                 parent::__construct();
                 $this->load->library('ion_auth');
+                $this->load->library('email');
                 $this->load->helper('url_helper');
 				$this->load->helper('url');
 				$this->load->helper(array('form', 'url'));
 				$this->load->model('banda_model');
 				$this->load->model('lanzamiento_model');
-				
         }
 
         public function index()
@@ -78,5 +78,56 @@ class Paginas extends CI_Controller {
 				$this->load->view('paginas/buscar', $data);
 				$this->load->view('templates/footer', $data);			        
 
+		}
+
+		public function send($value='')
+		{
+	        $this->load->library('form_validation');
+	        // field name, error message, validation rules
+	        $this->form_validation->set_rules('name', 'Nombre', 'trim|required');     
+	        $this->form_validation->set_rules('email', 'Correo', 'trim|required|valid_email');
+	        $this->form_validation->set_rules('comment', 'Comentario', 'trim|required');
+	                   
+	        if($this->form_validation->run() == FALSE) {
+	          	$this->index();
+	        } else {        
+	            $name = $this->input->post('name');
+	            $email = $this->input->post('email');
+	            $comment = $this->input->post('comment');            
+	            if(!empty($email)) {
+	                // send mail
+	                $config = array (
+	                  'mailtype' => 'html',
+	                  'charset'  => 'utf-8',
+	                  'priority' => '1'
+	                );
+	                $message='';
+	                $bodyMsg = '<p style="font-size:14px;font-weight:normal;margin-bottom:10px;margin-top:0;">'.$comment.'</p>';   
+	                $delimeter = $name."<br>".$contact_no;
+	                $dataMail = array('topMsg'=>'Hi Team', 'bodyMsg'=>$bodyMsg, 'thanksMsg'=>'Best regards,', 'delimeter'=> $delimeter);
+	 
+	                $this->email->initialize($config);
+	                $this->email->from($email, $name);
+	                $this->email->to(TO_MAIL);
+	                $this->email->subject('Contact Form');
+	                $message = $this->load->view('mailTemplate/contactForm', $dataMail, TRUE);
+	                $this->email->message($message);
+	                $this->email->send();
+	 
+	                // confirm mail
+	                $bodyMsg = '<p style="font-size:14px;font-weight:normal;margin-bottom:10px;margin-top:0;">Thank you for contacting us.</p>';                 
+	                $dataMail = array('topMsg'=>'Hi '.$name, 'bodyMsg'=>$bodyMsg, 'thanksMsg'=>'Best regards,', 'delimeter'=> 'Team TechArise');
+	 
+	                $this->email->initialize($config);
+	                $this->email->from(TO_MAIL, FROM_TEXT);
+	                $this->email->to($email);
+	                $this->email->subject('Contact Form Confimation');
+	                $message = $this->load->view('mailTemplate/contactForm', $dataMail, TRUE);
+	                $this->email->message($message);
+	                $this->email->send();                
+	            }
+	            $this->session->set_flashdata('msg', 'Thank you for your message. It has been sent.');
+	            redirect('paginas/');
+	        }
 		}
 }
--- application/views/paginas/contact.php
+++ application/views/paginas/contact.php
@@ -5,3 +5,61 @@
 ¿Encontró algún dato que está mal? ¿Tiene algún disco que aún no está acá? ¿Nos quiere mentar la madre por casposos (<a href="https://www.youtube.com/watch?v=0la5DBtOVNI" target="_blank">si puede por favor evítelo</a>)?<br>
 Por favor, contáctenos acá:
 </p>
+
+<br>
+<div class="row">
+    <div class="col-lg-12">
+        <?php if(!empty($this->session->flashdata('msg'))){ ?>
+            <div class="alert alert-success">
+                <?php echo $this->session->flashdata('msg'); ?>
+            </div>        
+        <?php } ?>
+        <?php if(validation_errors()) { ?>
+          <div class="alert alert-danger">
+            <?php echo validation_errors(); ?>
+          </div>
+        <?php } ?>
+    </div>
+</div>
+ <form action="<?php echo site_url('paginas/send');?>" method="POST" class="add-emp" id="add-emp">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="fa fa-user-o"></i>
+                    </span>
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Nombre completo">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="fa fa-envelope"></i>
+                    </span>
+                    <input type="text" name="email" class="form-control" id="email" placeholder="Email">
+                </div>
+            </div>
+        </div> 
+        <div class="col-lg-12">
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="fa fa-comments"></i>
+                    </span>
+                    <textarea name="comment" cols="3" rows="5" class="form-control" id="comment" placeholder="Comentario"></textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 text-right">
+            <button type="reset" name="reset_add_emp" id="re-submit-emp" class="btn btn-danger"><i class="fa fa-undo"></i> Reset</button>
+            <button type="submit" name="add_emp" id="submit-emp" class="btn btn-primary"><i class="fa fa-paper-plane"></i> Send</button>
+        </div>
+    </div>  
+</form>
