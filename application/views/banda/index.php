a15e39d40141d87ff2e4da1534a8b58161a26a86
Sergio Mancera <sergiomanceranom@gmail.com>
2018-09-28 15:07:55 -0500

    Añadidos indices en los listados de las bandas 	modified:   application/views/banda/index.php 	modified:   application/views/bandalanzamiento/lanzamiento_bandas.php

--- application/views/banda/index.php
+++ application/views/banda/index.php
@@ -1,9 +1,14 @@
 <h2><?php echo $title; ?></h2>
 
 <ul>
-	
+	<?php $primera = "" ?>
 	<?php foreach ($banda as $banda_item): ?>
 		<?php if($banda_item['extranjera'] == 0 or $extranjera == 'true') { ?>
+			<?php if (mb_substr($banda_item['nombre'], 0, 1) !== $primera){
+				$primera = mb_substr($banda_item['nombre'], 0, 1);
+				echo "<b>".$primera."</b>";
+			}
+			?>
 			<li>	
 			<a href="<?php echo site_url('banda/'.$banda_item['nombrecorto']); ?>"><?php echo $banda_item['nombre']; ?></a>
 			<?php if ($banda_item['otros']){?>
--- application/views/bandalanzamiento/lanzamiento_bandas.php
+++ application/views/bandalanzamiento/lanzamiento_bandas.php
@@ -4,12 +4,18 @@
 
 <?php echo form_open('bandalanzamiento/create_lanzamiento'); ?>
 
-
+<?php $primera = "" ?>
 
 <?php foreach ($banda as $banda_item): ?>
 
 
-	<label for="nombre"><?php echo "\"".$banda_item['id']."\""; ?> - <?php echo $banda_item['nombre']; ?></label>
+	<?php if (mb_substr($banda_item['nombre'], 0, 1) !== $primera){
+		$primera = mb_substr($banda_item['nombre'], 0, 1);
+		echo "<b>".$primera."</b></br>";
+	}
+	?>
+
+	<label for="nombre"> - <?php echo $banda_item['nombre']; ?></label>
 	
 	
 	<input type="checkbox" name="bandas[]" value="<?php echo $banda_item['id']; ?>"
@@ -24,7 +30,7 @@
 
 <?php endforeach; ?>
 <input type="hidden" name="lanzamiento" value="<?php echo $lanzamiento_id; ?>" />
-<input type="submit" name="submit" value="Create news item" />
+<input type="submit" name="submit" value="Asignar" />
 </form>
 
 
